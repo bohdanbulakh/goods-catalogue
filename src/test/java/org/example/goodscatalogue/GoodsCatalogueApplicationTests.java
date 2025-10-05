c1f1577ecc22d4613f7315adc9b2e294eb45c64c
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-10-05 20:22:15 +0300

    Initial commit

---
+++ src/test/java/org/example/goodscatalogue/GoodsCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.goodscatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsCatalogueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
